5691a3bfe4d7771fee74a718569fae77888d45a5
svetlana-aksenova <svetlana.aksenova.v@mail.ru>
2022-05-08 11:20:37 +0300

    Homework 8 task 59 fixed

--- src/homework8/task59/Main.java
+++ src/homework8/task59/Main.java
@@ -1,28 +1,13 @@
 package homework8.task59;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ExecutorService executor = Executors.newFixedThreadPool(3);
-        List<Future<String>> list = new ArrayList<Future<String>>();
-        Callable<String> callable = new RandomNumber();
-        for(int i=0; i< 10; i++) {
-            Future<String> future = executor.submit(callable);
-            list.add(future);
-        }
-        for(Future<String> fut : list){
-            try {
-                System.out.println(fut.get());
-            } catch (InterruptedException | ExecutionException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
-        executor.shutdown();
+        TreadPool.begin();
+
     }
 
 }
